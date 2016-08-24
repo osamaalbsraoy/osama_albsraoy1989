fc6720f53d81b42c508f5554361598c71b3c5a0f
osamaalbsraoy <iraq_new2016@mail.ru>
2016-08-24 14:21:09 +0300

    Update sticker.lua

--- plugins/sticker.lua
+++ plugins/sticker.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : osama < @os_aa23 >      #Dev
+# our channel: @os_aa23
 ]]
 local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
