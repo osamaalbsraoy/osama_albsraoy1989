6ab2a837fb8a6f5276694cfd9cb1f8b9ef2120f9
osamaalbsraoy <iraq_new2016@mail.ru>
2016-08-24 14:22:31 +0300

    Update welcome.lua

--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : osama < @os_aa23 >      #Dev
+# our channel: @os_aa23
 ]]
 
 
@@ -71,7 +71,7 @@ local function description_rules(msg, nama)
       local text = text.."                                               "
       local text = text.."Out of the group kickme ☹️"
       local text = text.."\n"
-      local text = text.."CHANNEL BOT : @help_tele"
+      local text = text.."CHANNEL BOT : @os_aa23"
       local receiver = get_receiver(msg)
       send_large_msg(receiver, text, ok_cb, false)
    end
