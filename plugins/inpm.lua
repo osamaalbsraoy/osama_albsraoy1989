40451f59ccf08c4ea8c0191fc5ae6ba4bc3faf04
osamaalbsraoy <iraq_new2016@mail.ru>
2016-08-24 14:08:07 +0300

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : osama < @os_aa23 >      #Dev
+# our channel: @os_aa23
 ]]
 local function pre_process(msg)
 local to = msg.to.type
