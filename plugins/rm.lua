efa556dd3c8da6154e7fea29d5116245b47fcf42
osamaalbsraoy <iraq_new2016@mail.ru>
2016-08-24 14:18:11 +0300

    Update rm.lua

--- plugins/rm.lua
+++ plugins/rm.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : osama < @os_aa23 >      #Dev
+# our channel: @os_aa23
 ]]
 local function history(extra, suc, result)
   for i=1, #result do
