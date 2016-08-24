65a15db2703524a5b255a8820f7b12f8ad988400
osamaalbsraoy <iraq_new2016@mail.ru>
2016-08-24 14:09:50 +0300

    Update leave_ban.lua

--- plugins/leave_ban.lua
+++ plugins/leave_ban.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : osama < @os_aa23 >      #Dev
+# our channel: @os_aa23
 ]]
 local function run(msg, matches)
 	local data = load_data(_config.moderation.data)
