1a2aeec35ff34163c4e7ac354f943f919c847de1
osamaalbsraoy <iraq_new2016@mail.ru>
2016-08-24 14:08:38 +0300

    Update inrealm.lua

--- plugins/inrealm.lua
+++ plugins/inrealm.lua
@@ -1,10 +1,4 @@
---[[
-# For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
-]]
--- data saved to moderation.json
--- check moderation plugin
+
 do
 
 local function create_group(msg)
