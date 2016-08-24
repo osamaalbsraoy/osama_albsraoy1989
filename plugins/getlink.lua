2cff223569830654fdab93bcc49e2a545a0b4de9
osamaalbsraoy <iraq_new2016@mail.ru>
2016-08-24 14:04:06 +0300

    Update getlink.lua

--- plugins/getlink.lua
+++ plugins/getlink.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : OSAMA < @OS_AA23 >      #Dev
+# our channel: @OS_AA23
 ]]
 do
 
