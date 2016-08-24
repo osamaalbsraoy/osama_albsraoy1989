90a3797ab4b2cb1dcbf2a999c6b7f66ae8fe5415
osamaalbsraoy <iraq_new2016@mail.ru>
2016-08-24 14:20:29 +0300

    Update set.lua

--- plugins/set.lua
+++ plugins/set.lua
@@ -1,7 +1,7 @@
 --[[
 # For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
+# Developer : osama < @os_aa23 >      #Dev
+# our channel: @os_aa23
 ]]
 local function save_value(msg, name, value)
   if (not name or not value) then
